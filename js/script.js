b77777081d2a2137f06e3ec69bbd11af01d721b2
ketevaniveshaguri <ketevaniveshaguri@gmail.com>
2023-01-25 14:29:43 +0400

    finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-const questions = document.querySelector(".question");
+const answer = document.querySelectorAll(".answer");
+
+document.querySelectorAll(".question").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    item.classList.toggle("open");
+    item.nextElementSibling.classList.toggle("open");
+    question.style.fontWeight = "700";
+    iconArrow.style.transform = "rotate(180deg)";
+  });
+});
